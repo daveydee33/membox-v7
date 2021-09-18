4e6cf3a3bb320599180201868f68bfd94bd2642b
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-09-18 11:55:59 +0800

    get items from fake db, dispatch, retrieve, and display

--- client/src/views/dave/index.js
+++ client/src/views/dave/index.js
@@ -1,5 +1,28 @@
-const index = () => {
-  return <div>blah</div>
+import { Fragment, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getItems } from './store/actions'
+
+const ItemsPage = () => {
+  // ** Vars
+  const dispatch = useDispatch()
+  const store = useSelector((state) => state.items)
+
+  // ** Get items
+  useEffect(() => {
+    dispatch(getItems())
+  }, [dispatch])
+
+  return (
+    <Fragment>
+      <div>blah</div>
+      {store.items.map((item) => (
+        <div>
+          <h2>{item.word}</h2>
+          <h4>{item.meaning}</h4>
+        </div>
+      ))}
+    </Fragment>
+  )
 }
 
-export default index
+export default ItemsPage
