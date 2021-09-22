64b35a6557c2b2e6efc323fe246bc4069c995008
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-09-22 15:47:07 +0800

    basic express server req/res works

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 
 const PORT = 4000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status('200').send({ oh: 'yea' })
+})
 
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
