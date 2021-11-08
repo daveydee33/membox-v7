84aa2bcd57e6528863b32da7bbe5b8baf12c563c
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-08 12:40:07 +0800

    duplicate @core Navbar so that I can customize it

--- client/src/layouts/VerticalLayout.js
+++ client/src/layouts/VerticalLayout.js
@@ -3,13 +3,13 @@ import Layout from '@layouts/VerticalLayout'
 
 // ** Components
 // import CustomMenu from './components/Menu'
-// import CustomNavbar from './components/Navbar'
+import CustomNavbar from '../components/navbar'
 // import CustomFooter from './components/Footer'
 
-const VerticalLayout = props => (
+const VerticalLayout = (props) => (
   <Layout
     // menu={props => <CustomMenu {...props} />}
-    // navbar={props => <CustomNavbar {...props} />}
+    navbar={(props) => <CustomNavbar {...props} />}
     // footer={props => <CustomFooter {...props} />}
     {...props}
   >
