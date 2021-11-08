14490af9cc5265f5baa2a46f71c34c52dc5823c6
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-08 11:38:38 +0800

    add firebase and add to Login page.  WIP, no error handling, no firebaseui, no state save

---
+++ client/src/firebase.js
@@ -0,0 +1,47 @@
+import { initializeApp } from 'firebase/app'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from 'firebase/auth'
+import { useEffect, useState } from 'react'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth()
+
+export function signup(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password)
+}
+
+export function login(email, password) {
+  return signInWithEmailAndPassword(auth, email, password)
+}
+
+export function logout() {
+  return signOut(auth)
+}
+
+// Custom Hook
+export function useAuth() {
+  const [currentUser, setCurrentUser] = useState()
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => setCurrentUser(user))
+    return unsub
+  }, [])
+
+  return currentUser
+}
