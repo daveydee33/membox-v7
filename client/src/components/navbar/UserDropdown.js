352f0eea09518bca1548e8c6fbb0474811c6d9f2
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-09 22:54:40 +0800

    add link to attribute back to avoid client log warnings

--- client/src/components/navbar/UserDropdown.js
+++ client/src/components/navbar/UserDropdown.js
@@ -15,22 +15,8 @@ import { useDispatch } from 'react-redux'
 import { handleLogout } from '@store/actions/auth'
 
 // ** Third Party Components
-import {
-  UncontrolledDropdown,
-  DropdownMenu,
-  DropdownToggle,
-  DropdownItem
-} from 'reactstrap'
-import {
-  User,
-  Mail,
-  CheckSquare,
-  MessageSquare,
-  Settings,
-  CreditCard,
-  HelpCircle,
-  Power
-} from 'react-feather'
+import { UncontrolledDropdown, DropdownMenu, DropdownToggle, DropdownItem } from 'reactstrap'
+import { User, Mail, CheckSquare, MessageSquare, Settings, CreditCard, HelpCircle, Power } from 'react-feather'
 
 // ** Default Avatar Image
 import defaultAvatar from '@src/assets/images/dave/blank_profile.png'
@@ -56,12 +42,7 @@ const UserDropdown = () => {
 
   return (
     <UncontrolledDropdown tag="li" className="dropdown-user nav-item">
-      <DropdownToggle
-        href="/"
-        tag="a"
-        className="nav-link dropdown-user-link"
-        onClick={(e) => e.preventDefault()}
-      >
+      <DropdownToggle href="/" tag="a" className="nav-link dropdown-user-link" onClick={(e) => e.preventDefault()}>
         <div className="user-nav d-sm-flex d-none">
           <span className="user-name font-weight-bold">
             {/* {(userData && userData['username']) || 'John Doe'} */}
@@ -69,19 +50,19 @@ const UserDropdown = () => {
           </span>
           <span className="user-status">
             {/* {(userData && userData.role) || 'Admin'} */}
-          </span>
+            </span>
         </div>
         <Avatar img={userAvatar} imgHeight="40" imgWidth="40" status="online" />
       </DropdownToggle>
       <DropdownMenu right>
-        <DropdownItem tag={Link} onClick={loginWithGooglePopup}>
+        <DropdownItem tag={Link} to="#" onClick={loginWithGooglePopup}>
           <User size={14} className="mr-75" />
           <span className="align-middle">
             {currentUser ? 'Switch User' : 'Login'}
           </span>
         </DropdownItem>
         {currentUser && (
-          <DropdownItem tag={Link} onClick={() => logout()}>
+          <DropdownItem tag={Link} to="#" onClick={() => logout()}>
             <Power size={14} className="mr-75" />
             <span className="align-middle">Logout</span>
           </DropdownItem>
