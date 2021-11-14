9546ee893901a42786a710f3d1339dda1463f4a8
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-15 00:39:41 +0800

    testing other method

--- server/src/config/firebase.js
+++ server/src/config/firebase.js
@@ -1,11 +1,6 @@
 const admin = require('firebase-admin');
 
-let serviceAccount;
-if (process.env.NODE_ENV !== 'production') {
-  serviceAccount = require('../../../secrets/google-credentials.json');
-} else {
-  serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-}
+const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
