5c049bc0d20e06cf20de92247245d402cb5a88b3
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-14 23:48:19 +0800

    try to handle credentials in prod and dev

--- server/src/config/firebase.js
+++ server/src/config/firebase.js
@@ -1,6 +1,11 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../../../secrets/google-credentials.json');
+let serviceAccount;
+if (process.env.NODE_ENV !== 'production') {
+  serviceAccount = require('../../../secrets/google-credentials.json');
+} else {
+  serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
