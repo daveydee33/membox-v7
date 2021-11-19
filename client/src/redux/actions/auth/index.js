116af0c6e308cd82c2ab5e4c5cc921be042f1360
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-19 23:33:00 +0800

    commenting out some redux stuff

--- client/src/redux/actions/auth/index.js
+++ client/src/redux/actions/auth/index.js
@@ -5,22 +5,22 @@ import axios from 'axios'
 const config = useJwt.jwtConfig
 
 // ** Handle User Login
-export const handleLogin = (data) => {
-  return (dispatch) => {
-    dispatch({
-      type: 'LOGIN',
-      data: data.user,
-      config,
-      [config.storageTokenKeyName]: data[config.storageTokenKeyName],
-      [config.storageRefreshTokenKeyName]: data[config.storageRefreshTokenKeyName]
-    })
+// export const handleLogin = (data) => {
+//   return (dispatch) => {
+//     dispatch({
+//       type: 'LOGIN',
+//       data: data.user,
+//       config,
+//       [config.storageTokenKeyName]: data[config.storageTokenKeyName],
+//       [config.storageRefreshTokenKeyName]: data[config.storageRefreshTokenKeyName]
+//     })
 
-    // ** Add to user, accessToken & refreshToken to localStorage
-    localStorage.setItem('userData', JSON.stringify(data.user))
-    localStorage.setItem(config.storageTokenKeyName, data.accessToken)
-    localStorage.setItem(config.storageRefreshTokenKeyName, data.refreshToken)
-  }
-}
+//     // ** Add to user, accessToken & refreshToken to localStorage
+//     localStorage.setItem('userData', JSON.stringify(data.user))
+//     localStorage.setItem(config.storageTokenKeyName, data.accessToken)
+//     localStorage.setItem(config.storageRefreshTokenKeyName, data.refreshToken)
+//   }
+// }
 
 // ** Handle User Logout
 export const handleLogout = () => {
