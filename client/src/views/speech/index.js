f03e210c8b3b13a1f5d3bd561be646de5574719d
Dave Degeatano <daveydee33@users.noreply.github.com>
2022-01-23 18:44:36 +0800

    slightly improved UI.  trying out Speakly polyfill to work in Firefox, but disabling it because it currently only works in English and Finnish

--- client/src/views/speech/index.js
+++ client/src/views/speech/index.js
@@ -1,6 +1,12 @@
-import { Card, CardHeader, CardBody, CardTitle, CardText, CardLink } from 'reactstrap'
-import { useContext, useEffect } from 'react'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill'
+import { Card } from 'reactstrap'
+
+/// Enable these 2 lines to use the Speechly Polyfill so that it works on Firefox and other browsers.
+///  But the problem is that it currently only supports English.  Keep watching the notes on react-speech-recognition and '@speechly/speech-recognition-polyfill' to see if it supports other languages later.
+// const appId = '__SPEECHLY_APP_ID_HERE__'
+// const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId)
+// SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition)
 
 const SpeechPage = () => {
   const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition, isMicrophoneAvailable } =
@@ -16,10 +22,14 @@ const SpeechPage = () => {
 
   const startListening = () => {
     resetTranscript()
-    SpeechRecognition.startListening({ continuous: true })
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: 'en-US' // 'en-US', 'id', etc.
+    })
   }
 
-  const fragments = ['hey', 'fine|well|good', "what's up"]
+  const fragments = ['hey|hi|hello', 'cambodia|america', 'thanks|thank you']
+  // const fragments = ['halo|hai', 'kamboja|amerik', 'terima kasih|makasih']
   const transcriptLowerCase = transcript.toLowerCase()
   const tests = fragments.map((fragment) => {
     const regex = new RegExp(`\\b(${fragment})\\b`)
@@ -27,10 +37,35 @@ const SpeechPage = () => {
   })
   const isAllTrue = tests.every((v) => v === true)
 
+  const listeningColor = listening ? 'red' : ''
+
   return (
     <Card>
-      <p>Microphone: {listening ? <span style={{ color: 'red' }}>on</span> : 'off'}</p>
+      {/* <p>Microphone: {listening ? <span style={{ color: 'red' }}>on</span> : 'off'}</p> */}
+
+      {/* <h3>Say Hi or Hello</h3>
+      <h3>Say where you are from [America, Cambodia]</h3>
+      <h3>Say thanks. [Thanks, Thank you]</h3> */}
+
+      {fragments.map((fragment) => {
+        return <h3>Say: {fragment.toUpperCase().replaceAll('|', ' or ')}</h3>
+      })}
+
+      {/* <button onClick={resetTranscript}>Reset</button> */}
+      <p>Transcript: {transcript}</p>
+      {fragments.map((fragment, index) => {
+        return (
+          <h2 key={fragment}>
+            {index + 1}: {tests[index] && '✅'}
+          </h2>
+          // <p key={fragment}>
+          //   Looking for: {fragment} : {tests[index] && 'YES!'}
+          // </p>
+        )
+      })}
+
       <button
+        style={{ backgroundColor: listeningColor }}
         onTouchStart={startListening}
         onMouseDown={startListening}
         onTouchEnd={SpeechRecognition.stopListening}
@@ -38,15 +73,8 @@ const SpeechPage = () => {
       >
         Hold to talk
       </button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>Transcript: {transcript}</p>
-      {fragments.map((fragment, index) => {
-        return (
-          <p key={fragment}>
-            Looking for: {fragment} : {tests[index] && 'YES!'}
-          </p>
-        )
-      })}
+
+      {isAllTrue && <h1>😎👍</h1>}
     </Card>
   )
 }
